6169e2ab9362f196953202df65b56b9e902e1e6d
Dave Connors <dave.connors@fishtownanalytics.com>
2022-01-03 14:23:52 -0600

    remove

--- models/audit/audit.sql
+++
@@ -1,9 +0,0 @@
-{% set old_etl_relation=ref('int_orders__pivoted_jinja') %}
-
-{% set dbt_relation=ref('int_orders__pivoted') %}
-
-{{ audit_helper.compare_relations(
-    a_relation=old_etl_relation,
-    b_relation=dbt_relation,
-    primary_key="order_id"
-) }}
